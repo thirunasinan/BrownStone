d6b75cebf305ebd0ed39a22b70261d8f784e52fa
vishnubalajikm <vishnubalaji.km@gmail.com>
2016-10-09 23:39:37 +0530

    Added Source in filter

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,16 +4,17 @@ class StudentsController < ApplicationController
 		subject = Subject
 		@subject = subject.all
 		@tags = subject.first.tags
+		@sources = subject.first.sources
 		@problems = Problem.all
 	end
 
 	def get_tags
-
-		render :json => {:tags => Subject.find(params[:id]).tags}
+		subject = Subject.find(params[:id])
+		render :json => {:tags => subject.tags, :sources => subject.sources}
 	end
 
 	def get_problems
-		@problems = Problem.joins(:tags).where("tags.id in (?) ", params[:id])
+		@problems = Problem.joins(:tags).where("tags.id in (?) and problems.source_id = ?", params[:tag_id] || [], params[:source_id])
 
 		render '_table', :layout => false 
 	end
